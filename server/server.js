513c5266c62b38b4ff01d72ff4d7ddea50cec631
Kaden Long <kglong20@gmail.com>
2022-02-23 13:56:38 -0700

    fixed error in server

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const path = require('path')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index'))
+    res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)}
-    )
+    console.log(`Listening on port ${port}`)
+})
